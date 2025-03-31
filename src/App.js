c7b7bda28f143d6f003255ba814e219ed85d8152
senyadigiter <senyadigiter@gmail.com>
2025-03-31 19:44:51 +0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import TokenTable from './TokenTable';
+import './styles.css';
+
+function App() {
+  return (
+    <div className="app">
+      <TokenTable />
+    </div>
+  );
+}
+
+export default App;
